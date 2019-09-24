826b87702da27e09c93c5f2d8f8fea6fb0cc757a
Kavian Darvish <kavian1991@gmail.com>
2019-09-24 09:56:41 -0400

    fixed some glitches so far so good

--- experiments/TicTacToe/script.js
+++ experiments/TicTacToe/script.js
@@ -1,72 +1,27 @@
-function init (){
-
-    var turn;
-    var wins;
-    const boxes = document.querySelectorAll('.box');
-
-    const winCard = document.querySelector('.winCard');
-    const loseCard = document.querySelector('.loseCard');
-    const resetButtons = document.querySelectorAll('button');
+// function init (){
 
 
 
-    function reset(){
+
+    // function reset(){
 
        
 
-        turn = 'x';
-        // this is all the winning posibilities
-        wins = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ];
-        boxes.forEach(box=>box.innerHTML = '');
-        winCard.classList.add('winCard') ;
-        loseCard.classList.add('loseCard') ;
-        resetButtons.forEach(button=>button.addEventListener('click',reset));
-    }
+    // }
     
 
 
 
-    function switchTurn(){
-        turn === 'x' ? turn ='o' : turn='x';
-        console.log('turn now is: '+turn)
-    }
+    
 
 
 
-    function assignTurn(wins,e){
-        console.log(turn,'ass')
-        for (i=0; i<wins.length; i++ ){
-            for (b=0; b<wins[i].length; b++){
-                if (wins[i][b] === Number(e.path[0]['id'][3])) {
-                    wins[i][b] = turn 
-                }
-            }
-        }
-        
-    }
+    
 
-    function winner(winner){
-        if (winner === 'x'){
-            winCard.classList.remove('winCard') ;
-        } 
-        else{
-            loseCard.classList.remove('loseCard') ;
-        }
-    }
-
-    function blockOrWin(){
-        console.log('block Or Win detected')
-        switchTurn();
-    }
+    // function blockOrWin(){
+        // console.log('block Or Win detected')
+        // switchTurn();
+    // }
 
 
 
@@ -78,121 +33,235 @@ function init (){
 
 
 
-    function ticMiddle(wins,e){
-        setTimeout(()=>{switchTurn(),console.log('after '+turn),assignTurn(wins,e,turn), boxes[4].innerHTML = turn.toUpperCase(); switchTurn() ; listenerEnabler(); return},1500);
-    }
+    // function ticMiddle(e,wins){
+        // setTimeout(()=>{switchTurn(),assignTurn(wins,e), boxes[4].innerHTML = turn.toUpperCase(); switchTurn() ; listenerEnabler(); return},1500);
+        // switchTurn()
+        // boxes[4].innerHTML = turn.toUpperCase();
+        // listenerEnabler()
+        // return false;
+        // console.log(turn,boxes[4].innerHTML)
+    // }
 
-    function ticCorner(boxes){
-        console.log('corner')
-        let random = Math.floor(Math.random() * Math.floor(4))
-        let corners = [0,2,6,8]
-        setTimeout(()=>{boxes[corners[random]].innerHTML = 'O'; listenerEnabler(); },1500);
+    // function ticCorner(boxes){
+        // console.log('corner')
+        // let random = Math.floor(Math.random() * Math.floor(4))
+        // let corners = [0,2,6,8]
+        // setTimeout(()=>{boxes[corners[random]].innerHTML = 'O'; listenerEnabler(); },1500);
 
-    }
+    // }
     
-    function ticAnyFree(boxes){
-        console.log(boxes)
+    // function ticAnyFree(boxes){
+        // console.log(boxes)
         // setTimeout(()=>{boxes[corners[random]].innerHTML = 'O'; switchTurn(); listenerEnabler(); },1500);
+    // }
+
+
+    // function isFree(){
+    //     var is = false;
+    //     wins.forEach((win)=>{
+    //         win.forEach(box=>{
+    //             if (typeof box === typeof 1){
+    //                 is = true;
+    //             }
+    //         })
+    //     })
+    //     return is;
+    // };
+
+
+
+
+
+
+
+
+    // function compTurn(e,boxes,wins){
+    //     chooseBox(wins,e,boxes);
+    // };
+
+
+
+
+        // function addMark(e){
+    //     
+            // listenerDisabler()
+    //         assignTurn(e,wins);
+    //         console.log(events,'first')
+    //         compTurn(events);
+    //     
+    //     }
+    // }
+
+
+
+
+// };
+
+
+
+const init = function(){
+
+    let turn;
+    let wins;
+    const boxes = document.querySelectorAll('.box');
+    const winCard = document.querySelector('.winCard');
+    const loseCard = document.querySelector('.loseCard');
+    const resetButtons = document.querySelectorAll('button');
+
+    const reset = function (){
+        turn = 'x';
+        wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+        boxes.forEach(box=>box.innerHTML = '');
+        winCard.classList.add('winCard') ;
+        loseCard.classList.add('loseCard') ;
+        resetButtons.forEach(button=>button.addEventListener('click',reset));
     }
 
+    const switchTurn = function (){
+        turn === 'x' ? turn ='o' : turn='x';
+    }
 
-    function isFree(){
-        var is = false;
-        wins.forEach((win)=>{
-            win.forEach(box=>{
-                if (typeof box === typeof 1){
-                    is = true
+    const writeOnTable = function(e){
+        e.path[0].innerHTML = turn.toUpperCase();
+    }
+
+    const writeToWinTable = function (e,wins){
+        for (i=0; i<wins.length; i++) {
+            for (b=0; b<wins[i].length; b++){
+                // console.log(wins[i][b],e.path[0]['id'][3],wins[i][b] === Number(e.path[0]['id'][3]))
+                if (wins[i][b] === Number(e.path[0]['id'][3])) {
+                     wins[i][b] = turn;
                 }
-            })
-        })
-        return is;
+            }
+        }
+
+    }
+
+    const lookForWinner = function(winner){
+        if (winner === 'x'){
+            winCard.classList.remove('winCard')
+        } else if (winner === 'o') {
+            loseCard.classList.remove('loseCard') ;
+        }
     }
 
 
 
-    function chooseBox(wins,e,boxes){
+    const computerTurn = function(e,wins) {
+        // loop around all the boxes and...
         for (i=0; i<wins.length; i++ ){
-            console.log(wins)
-            // 01. look for a winner:
-            if (wins[i][0] === wins[i][1] && wins[i][1] === wins[i][2]){
-                winner(wins[i][0]);
-                // return false;
-            }
+        // 01. look for a winner:
+        if(wins[i][0]===wins[i][1] && wins[i][1] === wins[i][2]){
+            lookForWinner(wins[i][1])
+        }
+            
+        }
+    }
+
+
+
+    // function chooseBox(e,boxes,wins){
+
+    //     
+
+                    // console.log(e)
+        // console.log(wins[i][0])
+        // console.log(wins[i][0] === wins[i][1])       
+        // console.log(wins[i][1] === wins[i][2])
+
+
+
+            // if (wins[i][0] === wins[i][1] && wins[i][1] === wins[i][2]){
+                // winner(wins[i][0]);
+            // return false;
+        // }
+
+        
+    // }
+
+
+
+
+
+        // return false;
+    // }
+            // ??????
+
+
 
 
             // 2 and 3 can be combined together !!!!!!!!!!!!!!!
             //  02. look out! do not lose, BLOCK!
             //  0.3 check to Selection, if you can win!
-            else if (
-                wins[i][0] === wins[i][1] || 
-                wins[i][0] === wins[i][2] ||
-                wins[i][1] === wins[i][2]
-                ) {
-                    blockOrWin()
-                }
+            // else if (
+            //     wins[i][0] === wins[i][1] || 
+            //     wins[i][0] === wins[i][2] ||
+            //     wins[i][1] === wins[i][2]
+            //     ) {
+            //         blockOrWin()
+            //     }
 
 
                 // 04. if middle is free, GET IT!
-            else if (typeof wins[1][1] === typeof 1){
-                    ticMiddle(wins,e)
-                }
+            // else if (typeof wins[1][1] === typeof 1){
+            //         ticMiddle(wins,e)
+            //     }
 
             
                 // 05.tic a corner
-            else if (typeof wins[0][0] === typeof 1 ||
-                     typeof wins[0][2] === typeof 1 ||
-                     typeof wins[2][0] === typeof 1 ||
-                     typeof wins[2][2] === typeof 1 
-                    ){
-                        ticCorner(boxes)
-                    }
+            // else if (typeof wins[0][0] === typeof 1 ||
+            //          typeof wins[0][2] === typeof 1 ||
+            //          typeof wins[2][0] === typeof 1 ||
+            //          typeof wins[2][2] === typeof 1 
+            //         ){
+            //             ticCorner(boxes)
+            //         }
                 // 06. cant? then tic anything,
                 // 7. cnat tic anything? reset!
-            else{
-                console.log('here')
-                if (isFree() === true){
-                    ticAnyFree(boxes)
-                } else{
-                    reset()
-                }
-            }
-            return false;
-        }
-    }
-
-    function compTurn(e,boxes){
-        chooseBox(wins,e,boxes);
-    };
+            // else{
+            //     if (isFree() === true){
+            //         ticAnyFree(boxes)
+            //     } else{
+            //         reset()
+            //     }
+            // }
+            // return false;
+        // }
+    // }
 
 
 
-    function addMark(e){
+
+
+
+
+
+    const addMark = function(e){
         if (e.path[0].innerHTML==='') {
-            e.path[0].innerHTML = turn.toUpperCase();
+            writeOnTable(e);
+            writeToWinTable(e,wins)
+            switchTurn()
             listenerDisabler()
-            assignTurn(wins,e);    
-            compTurn(e,boxes);
+            computerTurn(e,wins)
+            listenerEnabler()
         } else {
             alert('You cant mark here');
         }
     }
 
 
-    var listenerDisabler = () => {
-        boxes.forEach(box=>box.removeEventListener('click',addMark));
-    }
+
+
 
 
 
     const listenerEnabler=() => boxes.forEach(box=>box.addEventListener('click',addMark))
-    
-    
-    reset();
+    const listenerDisabler = () => {boxes.forEach(box=>box.removeEventListener('click',addMark))}
+
+    reset()
     listenerEnabler()
 
-};
 
+}
 
 init();
-
-
